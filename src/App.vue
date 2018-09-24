c5d111f138ad35aea7cfdd48f8c85fa4c71de6bf
bz.feng <6A4AE54A-4D96-43F7-BCEF-382A89C09138>
2018-09-24 21:50:30 +0800

    创建 git commit -m 创建

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header class="nav"></header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.nav{
+  width: 750px;
+  height: 80px;
+  background: red;
+}
+</style>
